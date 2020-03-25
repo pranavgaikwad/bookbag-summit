18c86fe9b9d2dce21f9460ba7a5cdecb48d638ca
Todd Sanders <tsanders@redhat.com>
2020-03-25 12:56:28 -0400

    Removing terminals and screen is not wide enough to have docs diaplyed properly.

--- envvars/gateway.sh
+++ envvars/gateway.sh
@@ -6,7 +6,7 @@
 #   split   - Split for two vertically stacked terminals
 #   split/2 - Split twice for three vertically stacked terminals
 #
-export TERMINAL_TAB='split'
+#export TERMINAL_TAB='split'
 
 # Disable OpenShift Console, this is not appropriate for Red Hat Summit as it
 # would point to the wrong cluster.
@@ -14,7 +14,7 @@ export TERMINAL_TAB='split'
 
 # Enable the following line to disable the right-hand pane entirely.
 # Only use this if you have no terminal, no slides, and no console.
-#export DEFAULT_PAGE='workshop'
+export DEFAULT_PAGE='workshop'
 
 # Flags to explicitly enable/disable specific components
 #ENABLE_CONSOLE='false'
